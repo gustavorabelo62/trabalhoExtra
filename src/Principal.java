107024e26329f6ad8330d37f49ae0850f7eb388e
gusta <gusta@192.168.11.6>
2017-11-22 23:44:32 -0300

    quase finalizado

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+   
+    public static void main(String[] args) {
+       
+        Contato c1 = new Contato("gustavo", "gustavorabelo62@yahoo.com.br", "99999999");
+        
+        
+    }
+    
+}
