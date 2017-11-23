18b25e93d31f9e921a73a6ee35fccbb6f13badb3
gusta <gusta@192.168.11.6>
2017-11-22 23:53:43 -0300

    nao pute fazer a parte 7 a 10 hoje se possivel mandare amanha

--- src/Principal.java
+++ src/Principal.java
@@ -5,7 +5,11 @@ public class Principal {
     public static void main(String[] args) {
        
         Contato c1 = new Contato("gustavo", "gustavorabelo62@yahoo.com.br", "99999999");
+        Consultar s1 = new Consultar();
         
+        s1.cadastrarContato(c1);
+
+        System.out.println(c1);
         
     }
     
